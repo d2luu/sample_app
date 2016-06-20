f6d1b975b77478033cd73abceb58e907788d3703
Dao Danh Luu <luubkk57@gmail.com>
2016-06-20 16:08:52 +0700

    finish user signup chap 7

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,23 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+  validates :dateofbirth, presence: true
+  validates :gender, presence: true
+  validate :checkbirthday
+  VALID_PHONE_NUMBER_REGEX = /0[0-9]{9,10}/
+  validates :phone, presence: true, length: {minmum: 10, maximum: 11}, format:{with: VALID_PHONE_NUMBER_REGEX}
+  # validates :gender
+
+  private
+  def checkbirthday
+  	if !self.dateofbirth.nil?
+  		if self.dateofbirth > Time.now
+  			errors.add(:dateofbirth, "Cannot be in the Future")
+  		elsif  self.dateofbirth < Time.now - 100.years
+  			errors.add(:dateofbirth, "Cannot be over 100 years ago")
+  		end
+  	end
+  end
+
+  				
 end
